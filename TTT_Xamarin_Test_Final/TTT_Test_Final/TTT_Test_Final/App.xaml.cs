2d08d123ab2b81c22a8f3edeece00aae290d9d8b
Sander Elsen <sander.elsen@pandora.be>
2016-10-28 08:59:27 +0200

    xamarin final test initial

---
+++ TTT_Xamarin_Test_Final/TTT_Test_Final/TTT_Test_Final/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TTT_Test_Final
+{
+    public partial class App : Application
+    {
+        private Device_Scan device_Scan;
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new TTT_Test_Final.Device_Scan();
+            
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ TTT_Xamarin_Test_Final/TTT_Test_Final/TTT_Test_Final/Device_Scan.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TTT_Test_Final
+{
+    public partial class Device_Scan : ContentPage
+    {
+        public Device_Scan()
+        {
+            InitializeComponent();
+        }
+    }
+}
