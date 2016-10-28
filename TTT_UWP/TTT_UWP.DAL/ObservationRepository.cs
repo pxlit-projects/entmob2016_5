6466daf0cb61a38ca0bb914a412f782d021216e5
laurenskvh <lakvh@hotmail.com>
2016-10-28 13:51:03 +0200

    Air pressure page + bugfix van double display ipv int

--- TTT_UWP/TTT_UWP.DAL/ObservationRepository.cs
+++ TTT_UWP/TTT_UWP.DAL/ObservationRepository.cs
@@ -60,18 +60,18 @@ namespace TTT_UWP.DAL
         {
             observations = new List<Observation>
              {
-                new Observation { ObservationID = 1, Temperature = 20, RegionID = 1, Humidity=5, AirPressure=10 },
-                new Observation { ObservationID = 2, Temperature = 21, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 3, Temperature = 18, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 4, Temperature = 24, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 5, Temperature = 20, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 6, Temperature = 23, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 7, Temperature = 24, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 8, Temperature = 21, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 9, Temperature = 24, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 10, Temperature = 28, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 11, Temperature = 24, RegionID = 1, Humidity=5, AirPressure=10  },
-                new Observation { ObservationID = 12, Temperature = 15 , RegionID = 1, Humidity=5, AirPressure=10 }
+                new Observation { ObservationID = 1, Temperature = 20.0, RegionID = 1, Humidity=5, AirPressure=11.9, Timestamp = DateTime.Now },
+                new Observation { ObservationID = 2, Temperature = 21.0, RegionID = 1, Humidity=5, AirPressure=12.8, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 3, Temperature = 18.1, RegionID = 2, Humidity=5, AirPressure=13.7, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 4, Temperature = 24.2, RegionID = 2, Humidity=5, AirPressure=10.5, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 5, Temperature = 20.2, RegionID = 3, Humidity=5, AirPressure=12.4, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 6, Temperature = 23.6, RegionID = 3, Humidity=5, AirPressure=11.3, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 7, Temperature = 24.1, RegionID = 4, Humidity=5, AirPressure=10.2, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 8, Temperature = 21.2, RegionID = 4, Humidity=5, AirPressure=13.2, Timestamp = DateTime.Now  },
+                new Observation { ObservationID = 9, Temperature = 24.2, RegionID = 5, Humidity=5, AirPressure=14.2, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 10, Temperature = 28.2, RegionID = 5, Humidity=5, AirPressure=50.2, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 11, Temperature = 24.8, RegionID = 6, Humidity=5, AirPressure=11.2, Timestamp = DateTime.Now   },
+                new Observation { ObservationID = 12, Temperature = 15.9 , RegionID = 6, Humidity=5, AirPressure=10.2, Timestamp = DateTime.Now  }
              };
         }
     }
--- TTT_UWP/TTT_UWP/ViewModels/PressurePageViewModel.cs
+++ TTT_UWP/TTT_UWP/ViewModels/PressurePageViewModel.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TTT_UWP.DAL;
+using TTT_UWP.Model;
+using TTT_UWP.Services;
+using TTT_UWP.Utility;
 
 namespace TTT_UWP.ViewModels
 {
     class PressurePageViewModel
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Redirect
+        private INavigationService navigationService;
+        public ICommand RedirectCommand { get; set; }
+        public ICommand GoBackCommand { get; set; }
+
+        public IObservationRepository observationRepository;
+        public ObservableCollection<Observation> observations = new ObservableCollection<Observation>();
+
+        public PressurePageViewModel(INavigationService navigationService)
+        {
+            observationRepository = new ObservationRepository();
+            AddDummyData();
+            RedirectCommand = new CustomCommand(OnRedirect, CanRedirect);
+            GoBackCommand = new CustomCommand(OnGoBack, CanRedirect);
+            this.navigationService = navigationService;
+        }
+
+        private void OnRedirect(object o)
+        {
+            navigationService.Navigate(TypeHelper.GetTypeByString(o.ToString(), this.GetType().GetTypeInfo().Assembly));
+        }
+
+        private void OnGoBack(object o)
+        {
+            navigationService.GoBack();
+        }
+
+        //Mag er geredirect worden (huidig altijd true)
+        private bool CanRedirect(object o)
+        {
+            return true;
+        }
+
+        public void AddDummyData()
+        {
+            foreach (Observation item in observationRepository.GetObservations())
+            {
+                observations.Add(item);
+            }
+        }
+
+        public ObservableCollection<Observation> Observations
+        {
+            get
+            {
+                return observations;
+            }
+            set
+            {
+                observations = value;
+                RaisePropertyChanged("Observations");
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
