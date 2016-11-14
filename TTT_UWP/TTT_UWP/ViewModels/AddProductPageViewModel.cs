cb6bca3b24daac90144ed8e8380a251380542eba
laurenskvh <lakvh@hotmail.com>
2016-11-14 16:10:14 +0100

    Add product werkt semi, maak morgen verder

--- TTT_UWP/TTT_UWP/ViewModels/AddProductPageViewModel.cs
+++ TTT_UWP/TTT_UWP/ViewModels/AddProductPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -26,11 +27,15 @@ namespace TTT_UWP.ViewModels
 
         //Databinding
         public event PropertyChangedEventHandler PropertyChanged;
-        private Product productToAdd;
+        private Product productToAdd = new Product();
 
         //Commands
         public ICommand AddCommand { get; set; }
         public ICommand CancelCommand { get; set; }
+
+        //Bullshit mvvm shit nest fuck
+        private string productName;
+        private string productMaxTemperature, productMinTemperature, productMaxHumidity, productMinHumidity, productMaxAirPressure, productMinAirPressure;
 
         public AddProductPageViewModel(INavigationService navigationService)
         {
@@ -46,13 +51,14 @@ namespace TTT_UWP.ViewModels
 
         private void OnAddProduct(object obj)
         {
+            Product p = productToAdd;
             /*
             PropertyInfo productProperty = obj.GetType().GetProperty("Product");
             Product product = (Product)productProperty.GetValue(obj);
 
             product.ProductName = "foo";
 
-            */
+            
             var objType = this.GetType();
             List<PropertyInfo> properties = new List<PropertyInfo>();
 
@@ -69,7 +75,7 @@ namespace TTT_UWP.ViewModels
                 }
             }
 
-            /*
+            
             
             Boolean somethingNull = false;
             Object o2 = obj;
@@ -124,6 +130,111 @@ namespace TTT_UWP.ViewModels
             {
                 productToAdd = value;
                 RaisePropertyChanged("ProductToAdd");
+            }
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return productName;
+            }
+
+            set
+            {
+                productName = value;
+                productToAdd.ProductName = value;
+                RaisePropertyChanged("ProductName");
+            }
+        }
+
+        public string ProductMaxTemperature
+        {
+            get
+            {
+                return productMaxTemperature;
+            }
+
+            set
+            {
+                productMaxTemperature = value;
+                productToAdd.MaximumTemperature = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMaxTemperature");
+            }
+        }
+
+        public string ProductMinTemperature
+        {
+            get
+            {
+                return productMinTemperature;
+            }
+
+            set
+            {
+                productMinTemperature = value;
+                productToAdd.MinimumTemperature = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMinTemperature");
+            }
+        }
+
+        public string ProductMaxHumidity
+        {
+            get
+            {
+                return productMaxHumidity;
+            }
+
+            set
+            {
+                productMaxHumidity = value;
+                productToAdd.MaximumHumidity = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMaxHumidity");
+            }
+        }
+
+        public string ProductMinHumidity
+        {
+            get
+            {
+                return productMinHumidity;
+            }
+
+            set
+            {
+                productMinHumidity = value;
+                productToAdd.MinimumHumidity = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMinHumidity");
+            }
+        }
+
+        public string ProductMaxAirPressure
+        {
+            get
+            {
+                return productMaxAirPressure;
+            }
+
+            set
+            {
+                productMaxAirPressure = value;
+                productToAdd.MaximumAirPressure = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMaxAirPressure");
+            }
+        }
+
+        public string ProductMinAirPressure
+        {
+            get
+            {
+                return productMinAirPressure;
+            }
+
+            set
+            {
+                productMinAirPressure = value;
+                productToAdd.MinimumAirPressure = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMinAirPressure");
             }
         }
     }
