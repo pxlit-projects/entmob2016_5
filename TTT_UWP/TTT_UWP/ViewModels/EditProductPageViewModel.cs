db15d19406e7aa411a1e665821c70c7499eed94d
laurenskvh <lakvh@hotmail.com>
2016-11-15 13:48:24 +0100

    Edit product varia, werkt semi

--- TTT_UWP/TTT_UWP/ViewModels/EditProductPageViewModel.cs
+++ TTT_UWP/TTT_UWP/ViewModels/EditProductPageViewModel.cs
@@ -25,18 +25,20 @@ namespace TTT_UWP.ViewModels
         private static IProductDataService productDataService = new ProductDataService(productRepository);
 
         //Databinding
-        private Product selectedProduct;
+        private Product selectedProduct = new Product();
+        private string productName, productMaxTemperature, productMinTemperature, productMaxHumidity, productMinHumidity, productMaxAirPressure, productMinAirPressure, productId;
         public event PropertyChangedEventHandler PropertyChanged;
 
         //Commands
         public ICommand EditCommand { get; set; }
         public ICommand CancelCommand { get; set; }
+        public ICommand GoBackCommand { get; set; }
 
         public EditProductPageViewModel(INavigationService navigationService)
         {
             LoadCommands();
-
-            Messenger.Default.Register<Product>(this, OnProductReceived);
+            
+            Messenger.Default.Register<Product>(this, (action) => OnProductReceived(action));
 
             this.navigationService = navigationService;
         }
@@ -44,12 +46,24 @@ namespace TTT_UWP.ViewModels
         private void OnProductReceived(Product product)
         {
             selectedProduct = product;
+            ProductName = selectedProduct.ProductName;
+            ProductId = Convert.ToString(selectedProduct.ProductID);
+
+            ProductMaxAirPressure = Convert.ToString(selectedProduct.MaximumAirPressure);
+            ProductMinAirPressure = Convert.ToString(selectedProduct.MinimumAirPressure);
+
+            ProductMaxHumidity = Convert.ToString(selectedProduct.MaximumHumidity);
+            ProductMinHumidity = Convert.ToString(selectedProduct.MinimumHumidity);
+
+            ProductMaxTemperature = Convert.ToString(selectedProduct.MaximumTemperature);
+            ProductMinTemperature = Convert.ToString(selectedProduct.MinimumTemperature);
         }
 
         private void LoadCommands()
         {
             EditCommand = new CustomCommand(OnEditProduct, CanRedirect);
             CancelCommand = new CustomCommand(OnGoBack, CanRedirect);
+            GoBackCommand = new CustomCommand(OnGoBack, CanRedirect);
         }
 
         private void OnEditProduct(object obj)
@@ -87,6 +101,126 @@ namespace TTT_UWP.ViewModels
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string ProductName
+        {
+            get
+            {
+                return productName;
+            }
+
+            set
+            {
+                productName = value;
+                selectedProduct.ProductName = value;
+                RaisePropertyChanged("ProductName");
+            }
+        }
+
+        public string ProductMaxTemperature
+        {
+            get
+            {
+                return productMaxTemperature;
+            }
+
+            set
+            {
+                productMaxTemperature = value;
+                selectedProduct.MaximumTemperature = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMaxTemperature");
+            }
+        }
+
+        public string ProductMinTemperature
+        {
+            get
+            {
+                return productMinTemperature;
+            }
+
+            set
+            {
+                productMinTemperature = value;
+                selectedProduct.MinimumTemperature = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMinTemperature");
+            }
+        }
+
+        public string ProductMaxHumidity
+        {
+            get
+            {
+                return productMaxHumidity;
+            }
+
+            set
+            {
+                productMaxHumidity = value;
+                selectedProduct.MaximumHumidity = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMaxHumidity");
+            }
+        }
+
+        public string ProductMinHumidity
+        {
+            get
+            {
+                return productMinHumidity;
+            }
+
+            set
+            {
+                productMinHumidity = value;
+                selectedProduct.MinimumHumidity = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMinHumidity");
+            }
+        }
+
+        public string ProductMaxAirPressure
+        {
+            get
+            {
+                return productMaxAirPressure;
+            }
+
+            set
+            {
+                productMaxAirPressure = value;
+                selectedProduct.MaximumAirPressure = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMaxAirPressure");
+            }
+        }
+
+        public string ProductMinAirPressure
+        {
+            get
+            {
+                return productMinAirPressure;
+            }
+
+            set
+            {
+                productMinAirPressure = value;
+                selectedProduct.MinimumAirPressure = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductMinAirPressure");
+            }
+        }
+
+        public string ProductId
+        {
+            get
+            {
+                return productId;
+            }
+
+            set
+            {
+                productId = value;
+                selectedProduct.ProductID = Convert.ToInt32(value);
+                RaisePropertyChanged("ProductId");
             }
         }
     }
